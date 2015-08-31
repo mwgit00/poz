611a6c25a7e4658fb7fd43fc0b7f35236d7dcb1f
Mark Whitney <mw0868@earthlink.net>
2015-08-30 22:20:29 -0500

    Clean-up
    Made some of the array structures simpler in the landmark calculation
    stuff.  Added some more experiments.

--- poz.py
+++ poz.py
@@ -4,6 +4,7 @@
 
 import math
 import numpy as np
+import cv2
 
 import pozutil as pu
 
@@ -22,8 +23,20 @@ def rot_pos_rpy_deg(xyz_pos, r, p, y):
     return np.dot(ro_mat, np.transpose(xyz_pos))
 
 
-if __name__ == "__main__":
+def project_points_cv(cam_mat, xyz_arr):
+    """Project 3D world point to image plane.
+    :param xyz_arr: array of points
+    """
+    #R = pu.calc_rot_mat(0*pu.DEG2RAD, 0*pu.DEG2RAD, 0.)
+    tvec = np.float32([0., 0., 0.])
+    #Rrod, _ = cv2.Rodrigues(R)
+    rvec = np.zeros((3, 1), dtype=np.float32)
+    aaaa, _ = cv2.projectPoints(xyz_arr, rvec, tvec, cam_mat, distCoeffs=None)
+    print aaaa
+    print
 
+
+if __name__ == "__main__":
     # TODO -- clean this up
 
     cam = pu.CameraHelper()
@@ -42,7 +55,7 @@ if __name__ == "__main__":
     print "Start with landmark at some XYZ"
     print "Camera elevation = ", 0.0
     print "Camera azimuth = ", 0.0
-    xyz = np.array([[0., landmark_y, arb_z]])
+    xyz = np.array([0., landmark_y, arb_z])
     print(xyz)
     print "Range to landmark = ", np.linalg.norm(xyz)
     u, v = cam.project_xyz_to_uv(np.transpose(xyz))
@@ -63,6 +76,9 @@ if __name__ == "__main__":
     print ("ele=", a_el, "azi=", a_az)
     print
 
+    #project_points_cv(cam.camA, np.transpose(xyzr))
+    #print
+
     print "Determine range to landmark using only (u, v)"
     xyz_ray = np.multiply(xyzr, (1. / xyzr[2]))
     print xyz_ray, "Normalize ray to landmark in image"
@@ -78,6 +94,20 @@ if __name__ == "__main__":
     print
 
     print "Using CameraHelper routine..."
-    rng, azi = cam.calc_range_to_landmark(xyz, u, v, arb_ele * pu.DEG2RAD)
+    rng, azi = cam.calc_rng_azi_to_landmark(xyz, u, v, arb_ele * pu.DEG2RAD)
     print "Range = ", rng
     print "Azimuth = ", azi * pu.RAD2DEG
+    print
+
+    print "Second landmark test..."
+    xyz2 = np.array([1.0, landmark_y, arb_z])
+    print xyz2
+    xyz2r = rot_axes_rpy_deg(xyz2, arb_ele, arb_azi, 0)
+    print xyz2r
+    u2, v2 = cam.project_xyz_to_uv(xyz2r)
+    print "Landmark #2 image coords = ", (u2, v2)
+    rng, azi = cam.calc_rng_azi_to_landmark(xyz2, u2, v2, arb_ele * pu.DEG2RAD)
+    print "Range = ", rng
+    print "Azimuth = ", azi * pu.RAD2DEG
+
+    print "Done."
